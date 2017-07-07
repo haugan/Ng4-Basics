126864dff83170dcd2c73c887301e24b5da5974f
haugan <hauganmarius@gmail.com>
2017-07-07 11:36:57 +0200

    Add button validation and click listener for disabled property

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,24 +10,20 @@ import { Component, OnInit } from '@angular/core';
   `]
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server is created.';
+  allowAddServer = false;
+  serverAddStatus = 'No server is created.';
   serverName = '';
 
   constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
   }
 
   ngOnInit() {
   }
 
-  onCreateServer() {
-    this.serverCreationStatus = `New server was created! (${this.serverName})`;
-  }
-
-  onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
+  onAddServer() {
+    this.serverAddStatus = `
+      New server was created! (${this.serverName})
+    `;
+    this.serverName = '';
   }
 }
